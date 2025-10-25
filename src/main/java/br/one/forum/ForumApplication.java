9fcdc4d3cb347f1dc632baa45e8ad01e5e57adc8
Swellington Soares <qb.corebr@gmail.com>
2025-10-25 16:19:13 -0300

    ops(config): remoção da auto configuração do security.

--- src/main/java/br/one/forum/ForumApplication.java
+++ src/main/java/br/one/forum/ForumApplication.java
@@ -1,10 +1,19 @@
 package br.one.forum;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.autoconfigure.security.servlet.ManagementWebSecurityAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
 
 
-@SpringBootApplication
+@SpringBootApplication(
+        exclude = {
+                SecurityAutoConfiguration.class,
+                ManagementWebSecurityAutoConfiguration.class,
+                UserDetailsServiceAutoConfiguration.class
+        }
+)
 public class ForumApplication {
     public static void main(String[] args) {
         SpringApplication.run(ForumApplication.class, args);
