dba68da70c10449224cae87aa4076f0b4dd2d531
Swellington Soares <qb.corebr@gmail.com>
2025-11-07 22:31:44 -0300

    fix(Comment): Adicionado as funções de Equal e HashCode e o construtor padrão.

--- src/main/java/br/one/forum/entities/Comment.java
+++ src/main/java/br/one/forum/entities/Comment.java
@@ -2,18 +2,21 @@ package br.one.forum.entities;
 
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotNull;
-import lombok.Getter;
-import lombok.Setter;
-import org.hibernate.annotations.ColumnDefault;
+import lombok.*;
 import org.hibernate.annotations.OnDelete;
 import org.hibernate.annotations.OnDeleteAction;
+import org.hibernate.proxy.HibernateProxy;
 
 import java.time.Instant;
+import java.util.Objects;
 
 @Getter
 @Setter
 @Entity
 @Table(name = "comments")
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString(exclude = {"topic", "user"})
 public class Comment {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -40,4 +43,25 @@ public class Comment {
     @Column(name = "created_at")
     private Instant createdAt = Instant.now();
 
+    public Comment(Topic topic, User user, String content) {
+        this.topic = topic;
+        this.user = user;
+        this.content = content;
+    }
+
+    @Override
+    public final boolean equals(Object object) {
+        if (this == object) return true;
+        if (object == null) return false;
+        Class<?> oEffectiveClass = object instanceof HibernateProxy ? ((HibernateProxy) object).getHibernateLazyInitializer().getPersistentClass() : object.getClass();
+        Class<?> thisEffectiveClass = this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass() : this.getClass();
+        if (thisEffectiveClass != oEffectiveClass) return false;
+        Comment comment = (Comment) object;
+        return getId() != null && Objects.equals(getId(), comment.getId());
+    }
+
+    @Override
+    public final int hashCode() {
+        return this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass().hashCode() : getClass().hashCode();
+    }
 }
