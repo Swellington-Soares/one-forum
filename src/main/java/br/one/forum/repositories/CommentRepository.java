a7b567dde53761e2ec080c16e313042439b9c50a
Swellington Soares <qb.corebr@gmail.com>
2025-11-07 21:21:11 -0300

    chore: Criação inicial dos repositórios.

---
+++ src/main/java/br/one/forum/repositories/CommentRepository.java
@@ -0,0 +1,13 @@
+package br.one.forum.repositories;
+
+import br.one.forum.entities.Comment;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface CommentRepository extends JpaRepository<Comment, Integer> {
+    List<Comment> findByUserId(Integer userId);
+    List<Comment> findByTopicId(Integer topicoId);
+}
---
+++ src/main/java/br/one/forum/repositories/TopicRepository.java
@@ -0,0 +1,13 @@
+package br.one.forum.repositories;
+
+import br.one.forum.entities.Topic;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface TopicRepository extends JpaRepository<Topic, Integer> {
+    List<Topic> findByUserId(Integer userId);
+    List<Topic> findByTitleContainingIgnoreCase(String title);
+}
