d2bb177e7f84e55b493b019d66d9f054d6f47dd8
Swellington Soares <qb.corebr@gmail.com>
2025-11-07 22:31:58 -0300

    fix(Profile): Adicionado as funções de Equal e HashCode e o construtor padrão.

--- src/main/java/br/one/forum/entities/Profile.java
+++ src/main/java/br/one/forum/entities/Profile.java
@@ -2,13 +2,19 @@ package br.one.forum.entities;
 
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotNull;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
+import org.hibernate.proxy.HibernateProxy;
+
+import java.util.Objects;
 
 @Getter
 @Setter
 @Entity
-@Table(name = "profile", schema = "forum")
+@Table(name = "profile")
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString(exclude = "user")
 public class Profile {
     @Id
     @Column(name = "user_id", nullable = false)
@@ -28,4 +34,25 @@ public class Profile {
     @Column(name = "photo")
     private String photo;
 
+
+    public Profile(User user, String name) {
+        this.user = user;
+        this.name = name;
+    }
+
+    @Override
+    public final boolean equals(Object object) {
+        if (this == object) return true;
+        if (object == null) return false;
+        Class<?> oEffectiveClass = object instanceof HibernateProxy ? ((HibernateProxy) object).getHibernateLazyInitializer().getPersistentClass() : object.getClass();
+        Class<?> thisEffectiveClass = this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass() : this.getClass();
+        if (thisEffectiveClass != oEffectiveClass) return false;
+        Profile profile = (Profile) object;
+        return getId() != null && Objects.equals(getId(), profile.getId());
+    }
+
+    @Override
+    public final int hashCode() {
+        return this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass().hashCode() : getClass().hashCode();
+    }
 }
