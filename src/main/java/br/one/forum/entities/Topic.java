93d8d8bec537f53c59ac33e9cc0da83e6b61f94d
Swellington Soares <qb.corebr@gmail.com>
2025-11-07 22:32:33 -0300

    fix(Topic): Adicionado as funções de Equal e HashCode e o construtor padrão e correção na lista de categorias.

--- src/main/java/br/one/forum/entities/Topic.java
+++ src/main/java/br/one/forum/entities/Topic.java
@@ -2,16 +2,21 @@ package br.one.forum.entities;
 
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotNull;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 import org.hibernate.annotations.ColumnDefault;
+import org.hibernate.proxy.HibernateProxy;
 
 import java.time.Instant;
 import java.util.HashSet;
+import java.util.Objects;
 import java.util.Set;
 
 @Getter
 @Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString(exclude = {"likedBy", "comments", "user"})
+@Builder
 @Entity
 @Table(name = "topic")
 public class Topic {
@@ -42,14 +47,13 @@ public class Topic {
     @Column(name = "updated_at")
     private Instant updatedAt;
 
-    @ManyToMany
+    @ManyToMany(fetch = FetchType.EAGER)
     @JoinTable(
             name = "category_has_topics",
             joinColumns = @JoinColumn(name = "topic_id"),
             inverseJoinColumns = @JoinColumn(name = "category_id")
     )
     private Set<Category> categories = new HashSet<>();
-
 
     @ManyToMany
     @JoinTable(
@@ -59,8 +63,28 @@ public class Topic {
     )
     private Set<User> likedBy = new HashSet<>();
 
-
     @OneToMany(mappedBy = "topic", cascade = CascadeType.ALL, orphanRemoval = true)
     private Set<Comment> comments = new HashSet<>();
 
+    public Topic(String title, String content, User user) {
+        this.title = title;
+        this.content = content;
+        this.user = user;
+    }
+
+    @Override
+    public final boolean equals(Object object) {
+        if (this == object) return true;
+        if (object == null) return false;
+        Class<?> oEffectiveClass = object instanceof HibernateProxy ? ((HibernateProxy) object).getHibernateLazyInitializer().getPersistentClass() : object.getClass();
+        Class<?> thisEffectiveClass = this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass() : this.getClass();
+        if (thisEffectiveClass != oEffectiveClass) return false;
+        Topic topic = (Topic) object;
+        return getId() != null && Objects.equals(getId(), topic.getId());
+    }
+
+    @Override
+    public final int hashCode() {
+        return this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass().hashCode() : getClass().hashCode();
+    }
 }
