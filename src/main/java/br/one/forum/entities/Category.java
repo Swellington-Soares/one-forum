1e7cccd6129462b9bdbfd632ff21e4136a9e76fd
Swellington Soares <qb.corebr@gmail.com>
2025-11-11 10:10:15 -0300

    fix(Category): adição do tratamento para deixar o nome em maiúsculo no setName.

--- src/main/java/br/one/forum/entities/Category.java
+++ src/main/java/br/one/forum/entities/Category.java
@@ -36,8 +36,12 @@ public final class Category {
     @Setter(AccessLevel.NONE)
     private Set<Topic> topics = new HashSet<>();
 
-    public Category(String name) {
-        this.name = name;
+    public Category(@NotNull String name) {
+        setName(name);
+    }
+
+    void setName(@NotNull String name) {
+        this.name = name.toUpperCase();
     }
 
     public void addTopics(List<Topic> topics) {
