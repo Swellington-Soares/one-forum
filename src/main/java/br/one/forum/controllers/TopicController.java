81e4a0063cef102f6b759e4ea39a51d889ee2e1b
Swellington Soares <qb.corebr@gmail.com>
2025-11-13 08:07:28 -0300

    chore: Adição de elementos de exemplos para futuras implementações pela equipe.

--- src/main/java/br/one/forum/controllers/TopicController.java
+++ src/main/java/br/one/forum/controllers/TopicController.java
@@ -1,14 +1,14 @@
 package br.one.forum.controllers;
 
 
+import br.one.forum.assemblers.TopicModelAssembler;
 import br.one.forum.dtos.TopicResponseDto;
-import br.one.forum.entities.Topic;
 import br.one.forum.mappers.TopicResponseMapper;
 import br.one.forum.repositories.UserRepository;
+import br.one.forum.services.TopicService;
 import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.Parameter;
-import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.RequiredArgsConstructor;
+import org.springframework.hateoas.EntityModel;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,17 +21,15 @@ public class TopicController {
 
     private final TopicResponseMapper topicResponseMapper;
     private final UserRepository userRepository;
+    private final TopicService topicService;
+    private final TopicModelAssembler topicModelAssembler;
 
     @GetMapping("/{id}")
     @Operation(summary = "Busca um tópico pelo ID")
-    TopicResponseDto getTopic(
-            @Parameter(
-                    description = "ID do tópico a ser buscado",
-                    example = "1",
-                    schema = @Schema(type = "integer")
-            )
-            @PathVariable("id") Topic topic) {
-        var user = userRepository.findById(11).orElseThrow();
-        return topicResponseMapper.toDto(topic, user);
+    public EntityModel<TopicResponseDto> getTopic(
+            @PathVariable("id") Integer topicId) {
+        var topic = topicService.findTopicById( topicId );
+        var topicResponse = topicResponseMapper.toDto(topic, null);
+        return topicModelAssembler.toModel(topicResponse);
     }
 }
