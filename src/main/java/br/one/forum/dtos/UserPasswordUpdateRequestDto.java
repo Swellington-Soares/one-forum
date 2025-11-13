160693f5b4fdfaf7fa57dbe4060b1539bbf8536d
Swellington Soares <qb.corebr@gmail.com>
2025-11-13 02:41:49 -0300

    chore: remoção de imports sem utilização.

--- src/main/java/br/one/forum/dtos/UserPasswordUpdateRequestDto.java
+++ src/main/java/br/one/forum/dtos/UserPasswordUpdateRequestDto.java
@@ -2,7 +2,6 @@ package br.one.forum.dtos;
 
 import br.one.forum.validation.StrongPassword;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 
 public record UserPasswordUpdateRequestDto(
         @NotBlank
