6dcb8de94c21d87f8f76eddd9a10d8cca0994c3b
Swellington Soares <qb.corebr@gmail.com>
2025-11-18 18:37:06 -0300

    FIX(Auth): Mudança no antigo padrão de binding

--- src/main/java/br/one/forum/services/AuthenticationService.java
+++ src/main/java/br/one/forum/services/AuthenticationService.java
@@ -2,33 +2,35 @@ package br.one.forum.services;
 
 import br.one.forum.dtos.AuthenticationRequestDto;
 import br.one.forum.dtos.UserRegisterRequestDto;
+import br.one.forum.entities.Profile;
 import br.one.forum.entities.User;
 import br.one.forum.exception.UserAlreadyRegisteredException;
 import br.one.forum.exception.UserNotFoundException;
 import br.one.forum.exception.UserPasswordNotMatchException;
 import br.one.forum.repositories.UserRepository;
 import br.one.forum.security.UserSecurityDetails;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class AuthenticationService {
 
-    @Autowired
-    private AuthenticationManager authenticationManager;
+    private final AuthenticationManager authenticationManager;
 
-    @Autowired
-    private UserRepository userRepository;
 
-    @Autowired
-    private TokenService tokenService;
+    private final UserRepository userRepository;
 
-    @Autowired
-    private PasswordEncoder passwordEncoder;
+    private final TokenService tokenService;
+
+    private final PasswordEncoder passwordEncoder;
+    private final UserService userService;
 
     public String login(AuthenticationRequestDto data) {
         var userNamePassword = new UsernamePasswordAuthenticationToken(data.email(), data.password());
@@ -53,8 +55,19 @@ public class AuthenticationService {
         }
 
         String encryptedPassword = passwordEncoder.encode(data.password());
-        User newUser = new User(data.email(), encryptedPassword);
+        User newUser = new User(
+                data.email(),
+                encryptedPassword,
+                new Profile(
+                        data.name(),
+                        data.avatarUrl()
+                ));
         userRepository.save(newUser);
         return true;
+    }
+
+    public User getLoggedUserByUserDetails(UserDetails userDetails) {
+        if (userDetails == null) return null;
+        return userService.findUserByEmailOrNull( userDetails.getUsername() );
     }
 }
