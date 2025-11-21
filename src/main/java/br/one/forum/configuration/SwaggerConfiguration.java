2ae045c6057ea52870e2e403646c9a02bdf52dee
Swellington Soares <qb.corebr@gmail.com>
2025-11-20 21:06:40 -0300

    chore: Adição da configuração de paginação no swagger.

--- src/main/java/br/one/forum/configuration/SwaggerConfiguration.java
+++ src/main/java/br/one/forum/configuration/SwaggerConfiguration.java
@@ -1,5 +1,9 @@
 package br.one.forum.configuration;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Contact;
@@ -11,6 +15,17 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@OpenAPIDefinition(
+        security = {
+                @SecurityRequirement(name = "bearerAuth")
+        }
+)
+@SecurityScheme(
+        name = "bearerAuth",
+        type = SecuritySchemeType.HTTP,
+        scheme = "bearer",
+        bearerFormat = "JWT"
+)
 public class SwaggerConfiguration {
 
     @Bean
