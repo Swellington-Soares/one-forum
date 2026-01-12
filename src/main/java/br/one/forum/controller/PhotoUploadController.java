27e0798753701c436ed6ddd7223fa261a28cd89f
Swellington Soares <qb.corebr@gmail.com>
2026-01-12 20:52:02 -0300

    fix: ajuste do tipo de post para put

--- src/main/java/br/one/forum/controller/PhotoUploadController.java
+++ src/main/java/br/one/forum/controller/PhotoUploadController.java
@@ -9,6 +9,7 @@ import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
@@ -20,7 +21,7 @@ public class PhotoUploadController {
     
     private final ProfileImageService processImageService;
 
-    @PostMapping
+    @PutMapping
     @PreAuthorize("isAuthenticated()")
     ResponseEntity<?> requestPhotoUpload(@RequestParam("file") MultipartFile file) {
         var user = ((AppUserDetailsInfo) SecurityContextHolder.getContext().getAuthentication().getPrincipal()).user();
